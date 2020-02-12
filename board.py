54b3d4241b96a0c436574f08dcceaf013a764548
veyzz <munir.taga@gmail.com>
2020-02-12 23:32:33 +0300

    added attribute, shows how many columns is filled

--- board.py
+++ board.py
@@ -2,13 +2,19 @@ class Board:
     def __init__(self, size):
         self.max_height = size[1]
         self.matrix = [[] for _ in range(size[0])]
+        self.filled = 0
 
     def push(self, n, el):
         '''Returns y coordinate of added element
+        or None if not succesfull
 
         '''
+        if n not in range(len(self.matrix)):
+            return None
         if len(self.matrix[n]) < self.max_height:
             self.matrix[n].append(el)
+            if len(self.matrix[n]) == self.max_height:
+                self.filled += 1
             return len(self.matrix[n]) - 1
         return None
 
@@ -24,4 +30,5 @@ class Board:
                 except IndexError:
                     s += '.'
             s += '\n'
+        s += ''.join(str(i) for i in range(len(self.matrix)))
         return s
